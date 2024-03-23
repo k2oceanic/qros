0eb8b1dd857b30a8459da280fc9aa87e7a339b0d
JakeBonney <jakebonney10@gmail.com>
2024-03-23 13:04:24 -0400

    Seperated qros.h into header/src file

--- qros/include/qros/qros.h
+++ qros/include/qros/qros.h
@@ -1,7 +1,5 @@
 #pragma once
 
-// #include "iocard_subscriber.h"
-// #include "hydraulic_card_subscriber.h"
 #include "qros_node.h"
 #include "qqml.h"
 #include "qros_string_subscriber.h"
@@ -13,17 +11,6 @@ QROS_NS_HEAD
 #define QML_PACKAGE_VERSION_MAJOR 1
 #define QML_PACKAGE_VERSION_MINOR 0
 
-void registerQmlTypes(){
-
-  qmlRegisterType<QRosNode> (QML_PACKAGE, QML_PACKAGE_VERSION_MAJOR,QML_PACKAGE_VERSION_MINOR,"QRosNode");
-  qRegisterMetaType<QRosNode*>("const QRosNode*");
-
-  qmlRegisterType<QRosStringSubscriber> (QML_PACKAGE, QML_PACKAGE_VERSION_MAJOR,QML_PACKAGE_VERSION_MINOR,"QRosStringSubscriber");
-  qRegisterMetaType<QRosStringSubscriber*>("const QRosStringSubscriber*");
-
-  qmlRegisterType<QRosStringPublisher> (QML_PACKAGE, QML_PACKAGE_VERSION_MAJOR,QML_PACKAGE_VERSION_MINOR,"QRosStringPublisher");
-  qRegisterMetaType<QRosStringPublisher*>("const QRosStringPublisher*");
-
-}
+void registerQmlTypes();
 
 QROS_NS_FOOT
