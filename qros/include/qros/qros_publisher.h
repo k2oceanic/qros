fb682c6881c1c663dd7f4c4d762da54d28009aa8
JakeBonney <jakebonney10@gmail.com>
2024-04-03 18:34:33 -0400

    made queue_size and latched qproperties

--- qros/include/qros/qros_publisher.h
+++ qros/include/qros/qros_publisher.h
@@ -43,29 +43,58 @@ class QRosPublisher : public QRosObject{
   Q_OBJECT
 public:
   Q_PROPERTY(QString topic READ getTopic WRITE setTopic NOTIFY topicChanged)
+  Q_PROPERTY(int queueSize READ getQueueSize WRITE setQueueSize NOTIFY queueSizeChanged)
+  Q_PROPERTY(bool latched READ isLatched WRITE setLatched NOTIFY latchedChanged)
+
 public slots:
-  void setTopic(QString topic, int queue_size = 10, bool latched = false){
-    try{
+  void setTopic(QString topic){
+    if (topic_ != topic) {
       topic_ = topic;
       interfacePtr()->setNode(getNode());
-      interfacePtr()->createRosPub(topic_, queue_size, latched); 
+      interfacePtr()->createRosPub(topic_, queue_size_, latched_);
       emit topicChanged();
-    }catch(...){
-      qWarning() << "invalid topic name: " << topic;
     }
   }
-  QString getTopic(){
+  QString getTopic() const {
     return topic_;
   }
+
+  void setQueueSize(int queueSize) {
+    if (queue_size_ != queueSize) {
+      queue_size_ = queueSize;
+      interfacePtr()->createRosPub(topic_, queue_size_, latched_);
+      emit queueSizeChanged();
+    }
+  }
+  int getQueueSize() const {
+    return queue_size_;
+  }
+
+  void setLatched(bool latched) {
+    if (latched_ != latched) {
+      latched_ = latched;
+      interfacePtr()->createRosPub(topic_, queue_size_, latched_);
+      emit latchedChanged();
+    }
+  }
+  bool isLatched() const {
+    return latched_;
+  }
+
   void publish(){
     interfacePtr()->publish();
   }
+
 signals:
   void topicChanged();
+  void queueSizeChanged();
+  void latchedChanged();
+
 protected:
   virtual QRosPublisherInterface * interfacePtr() = 0;
   QString topic_;
+  int queue_size_ = 10;
+  bool latched_ = false;
 };
-
 
 QROS_NS_FOOT
